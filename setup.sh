02abc53df82ae31af6d071bfbf1e616cc940c96f
Chetan Bhasin <connect@chetanbhasin.com>
2020-09-01 00:51:45 +0200

    Cleaned up some of the setup scripts

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -o vi 
+
 # Install Basic OS X Tools
 pushd osx
 sh install_osx.sh
@@ -7,16 +9,17 @@ popd
 
 # Install Rust and the tooling
 rustup install stable
-sh rust/setup.sh
+#sh rust/setup.sh
 
 #Install Powerline
 pushd powerline-fonts && ./install.sh && popd
 
 # Setup iTerm 2 configuration
-sudo cp conf/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
+mkdir -p ~/Library/Preferences/
+cp conf/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
 defaults read com.googlecode.iterm2
 
 # Setup Neovim
-sudo mkdir -p ~/.conf/nvim
-sudo cp conf/nvim_init.vim ~/.conf/nvim/init.vim
+mkdir -p ~/.conf/nvim
+cp conf/nvim_init.vim ~/.conf/nvim/init.vim
 
